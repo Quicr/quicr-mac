057f77c0de67ba5d74475ac2d935d0bfa4e22cd2
Rich Logan <rich@richlogan.co.uk>
2023-02-06 20:41:33 +0000

    Add framework identifier to build script

--- dependencies/build-qmedia-framework.sh
+++ dependencies/build-qmedia-framework.sh
@@ -12,17 +12,17 @@ CORES=$(getconf _NPROCESSORS_ONLN)
 
 # Build for catalyst
 mkdir -p $DIR/build-catalyst
-cmake -DCMAKE_FRAMEWORK=TRUE -DCMAKE_TOOLCHAIN_FILE=$DIR/ios.toolchain.cmake -S $DIR/new-qmedia -B $DIR/build-catalyst -DPLATFORM=MAC_CATALYST_ARM64 -DENABLE_VISIBILITY=ON
+cmake -DCMAKE_FRAMEWORK=TRUE -DCMAKE_TOOLCHAIN_FILE=$DIR/ios.toolchain.cmake -S $DIR/new-qmedia -B $DIR/build-catalyst -DPLATFORM=MAC_CATALYST_ARM64 -DENABLE_VISIBILITY=ON -DMACOSX_FRAMEWORK_IDENTIFIER=com.cisco.qmedia
 cmake --build $DIR/build-catalyst --target neo_media_client -j$CORES
 
 # Build for iOS
 mkdir -p $DIR/build-ios
-cmake -DCMAKE_FRAMEWORK=TRUE -DCMAKE_TOOLCHAIN_FILE=$DIR/ios.toolchain.cmake -S $DIR/new-qmedia -B $DIR/build-ios -DPLATFORM=OS64 -DENABLE_VISIBILITY=ON
+cmake -DCMAKE_FRAMEWORK=TRUE -DCMAKE_TOOLCHAIN_FILE=$DIR/ios.toolchain.cmake -S $DIR/new-qmedia -B $DIR/build-ios -DPLATFORM=OS64 -DENABLE_VISIBILITY=ON -DMACOSX_FRAMEWORK_IDENTIFIER=com.cisco.qmedia
 cmake --build $DIR/build-ios --target neo_media_client -j$CORES
 
 # Build for simulator
 mkdir -p $DIR/build-iossim
-cmake -DCMAKE_FRAMEWORK=TRUE -DCMAKE_TOOLCHAIN_FILE=$DIR/ios.toolchain.cmake -S $DIR/new-qmedia -B $DIR/build-iossim -DPLATFORM=SIMULATORARM64 -DENABLE_VISIBILITY=ON
+cmake -DCMAKE_FRAMEWORK=TRUE -DCMAKE_TOOLCHAIN_FILE=$DIR/ios.toolchain.cmake -S $DIR/new-qmedia -B $DIR/build-iossim -DPLATFORM=SIMULATORARM64 -DENABLE_VISIBILITY=ON -DMACOSX_FRAMEWORK_IDENTIFIER=com.cisco.qmedia
 cmake --build $DIR/build-iossim --target neo_media_client -j$CORES
 
 # Create xcframework
