0a80d29d8d39c617669ef088a6ab368a816bee27
trigaux <trigaux@cisco.com>
2023-02-28 16:18:50 -0800

    Adding quick option buttons for localhost and relay.

--- Decimus/Views/CallSetupView.swift
+++ Decimus/Views/CallSetupView.swift
@@ -17,7 +17,24 @@ struct CallSetupView: View {
         Text("Real Time Media Client").font(.title)
         Form {
             Section(header: Text("Join a meeting")) {
-                TextField("Address", text: $address, prompt: Text("Server Address"))
+                HStack {
+                    TextField("Address", text: $address, prompt: Text("Server Address")).disableAutocorrection(true)
+                    Divider()
+                    Button(action: {
+                        address = "127.0.0.1"
+                        port = 1234
+                    }, label: {
+                        Text("localhost")
+                    }).buttonStyle(.bordered)
+                    Button(action: {
+                        address = "relay.us-west-2.quicr.ctgpoc.com"
+                        port = 33434
+                    }, label: {
+                        Text("AWS")
+                    }).buttonStyle(.bordered)
+                }.alignmentGuide(.listRowSeparatorLeading) { _ in
+                    return 0
+                }
                 TextField("Port", value: $port, format: .number.grouping(.never), prompt: Text("Server Port"))
                 Button(action: join) {
                     Label("Join", systemImage: "phone")
