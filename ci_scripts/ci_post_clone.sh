84be43941c572aaf28c0055ca28a1abd46e113d7
Rich Logan <rich@richlogan.co.uk>
2023-03-10 16:06:19 +0000

    Build qmedia first

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# Skip package validation for build plugins.
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+
+# Build QMedia.
+./$CI_PROJECT_FILE_PATH/dependencies/build-qmedia-framework.sh
