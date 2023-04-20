0cfe0710d046e1e67ccbed9fe057cc625fea2656
Rich Logan <rich@richlogan.co.uk>
2023-04-20 09:35:19 +0100

    Cache native build

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -9,4 +9,7 @@ brew install pkg-config
 brew install go
 
 # Build QMedia.
+mv $CI_DERIVED_DATA_PATH/build-catalyst $CI_WORKSPACE/dependencies/build-catalyst
+mv $CI_DERIVED_DATA_PATH/build-catalyst-x86 $CI_WORKSPACE/dependencies/build-catalyst-x86
+mv $CI_DERIVED_DATA_PATH/build-ios $CI_WORKSPACE/dependencies/build-ios
 sh $CI_WORKSPACE/dependencies/build-qmedia-framework.sh
---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,4 @@
+# Cache native dependency build folders.
+mv $CI_WORKSPACE/dependencies/build-catalyst $CI_DERIVED_DATA_PATH/build-catalyst
+mv $CI_WORKSPACE/dependencies/build-catalyst-x86 $CI_DERIVED_DATA_PATH/build-catalyst-x86
+mv $CI_WORKSPACE/dependencies/build-ios $CI_DERIVED_DATA_PATH/build-ios
