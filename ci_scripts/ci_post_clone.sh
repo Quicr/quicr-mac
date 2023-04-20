1fe4f106e824f205c59bdf65a405d01a36bd43e0
Rich Logan <rich@richlogan.co.uk>
2023-04-20 10:26:47 +0100

    Verbose

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,9 +8,31 @@ brew install cmake
 brew install pkg-config
 brew install go
 
-# Build QMedia.
+# Restore native build caches from derived data.
 mkdir -p $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM
-[ -f $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-catalyst ] && mv $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-catalyst $CI_WORKSPACE/dependencies/build-catalyst
-[ -f $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-catalyst-x86 ] && mv $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-catalyst-x86 $CI_WORKSPACE/dependencies/build-catalyst-x86
-[ -f $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-ios ] && mv $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-ios $CI_WORKSPACE/dependencies/build-ios
+if [ -d $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-catalyst ]
+then
+    echo "Restoring build-catalyst from derived data"
+    mv $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-catalyst $CI_WORKSPACE/dependencies/build-catalyst
+else
+    echo "Couldn't find cached build-catalyst"
+fi
+
+if [ -d $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-catalyst-x86 ]
+then
+    echo "Restoring build-catalyst-x86 from derived data"
+    mv $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-catalyst-x86 $CI_WORKSPACE/dependencies/build-catalyst-x86
+else
+    echo "Couldn't find cached build-catalyst-x86"
+fi
+
+if [ -d $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-ios ]
+then
+    echo "Restoring build-ios from derived data"
+    mv $CI_DERIVED_DATA_PATH/$CI_PRODUCT_PLATFORM/build-ios $CI_WORKSPACE/dependencies/build-ios
+else
+    echo "Couldn't find cached build-ios"
+fi
+
+# Build QMedia.
 sh $CI_WORKSPACE/dependencies/build-qmedia-framework.sh
