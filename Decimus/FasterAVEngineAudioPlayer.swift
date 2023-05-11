65c4c6171cacb345014b612ddc5264677eccd0bf
Tomas Rigaux <tomas@rigaux.ca>
2023-05-11 10:53:48 -0700

    Revert "Protect element access with spinlock" (#91)
    * Revert "Don't fatal error on missing player"
    
    This reverts commit 5443ea81d6cf5b624c4e3e43a3b19a417b2774e4.
    
    * Revert "Protect element access with spinlock"
    
    This reverts commit e419144b81d98fc38a8dd9509a41437c27745c2e.

--- Decimus/FasterAVEngineAudioPlayer.swift
+++ Decimus/FasterAVEngineAudioPlayer.swift
@@ -4,22 +4,13 @@ import CTPCircularBuffer
 
 /// Plays audio samples out.
 class FasterAVEngineAudioPlayer: AudioPlayer {
-    private var engine: AVAudioEngine = .init()
-    private var mixer: AVAudioMixerNode = .init()
+    private var engine: AVAudioEngine! = .init()
+    private var mixer: AVAudioMixerNode! = .init()
     private let errorWriter: ErrorWriter
-    private var elements: [UInt32: Element] = [:]
-    private var elementsLock: atomic_flag = .init()
-    private var readContentions: Int32 = 0
-    private var writeContentions: Int32 = 0
+    private var players: [UInt32: AVAudioSourceNode] = [:]
+    private var buffers: [UInt32: TPCircularBuffer] = [:]
+    private var metrics: [UInt32: BufferMetrics] = [:]
 
-    /// Represents an individual input source for mixed output.
-    private struct Element {
-        var player: AVAudioSourceNode
-        var buffer: TPCircularBuffer
-        var metrics: BufferMetrics
-    }
-
-    /// Metrics.
     private struct BufferMetrics {
         var copyFails = 0
         var incompleteFrames = 0
@@ -37,222 +28,127 @@ class FasterAVEngineAudioPlayer: AudioPlayer {
     }
 
     deinit {
-        while Self.getLock(atomic: &elementsLock) == false {
-            // Spin.
-        }
-        defer { Self.unlock(atomic: &elementsLock)}
-
         engine.stop()
 
-        for identifier in elements.keys {
-            removeElement(identifier: identifier, lock: false)
+        for identifier in players.keys {
+            removePlayer(identifier: identifier)
         }
-        elements.removeAll()
+        players.removeAll()
 
         engine.disconnectNodeInput(mixer)
         engine.detach(mixer)
-    }
 
-    @inline(__always)
-    private static func getLock(atomic: inout atomic_flag) -> Bool {
-        atomic_flag_test_and_set_explicit(&atomic, memory_order_acquire) == false
-    }
-
-    @inline(__always)
-    private static func unlock(atomic: inout atomic_flag) {
-        atomic_flag_clear_explicit(&atomic, memory_order_release)
+        mixer = nil
+        engine = nil
     }
 
     func write(identifier: UInt32, buffer: AVAudioPCMBuffer) {
-        // Get the buffer for this stream and ensure it's valid.
-        var list = buffer.mutableAudioBufferList.pointee
-        guard list.mNumberBuffers == 1 else {
-            fatalError()
-        }
-        guard list.mBuffers.mDataByteSize > 0 else {
-            fatalError()
-        }
-
-        // Spin until we get access.
-        var contention = false
-        while Self.getLock(atomic: &elementsLock) == false {
-            if !contention {
-                OSAtomicIncrement32(&self.writeContentions)
-                contention = true
-            }
-        }
-        defer { Self.unlock(atomic: &elementsLock) }
-
-        // We got the lock, get data objects.
-        var intermediateElement: Element? = self.elements[identifier]
-        if intermediateElement == nil {
+        // Get the player node for this stream.
+        var node: AVAudioSourceNode? = players[identifier]
+        if node == nil {
             do {
-                intermediateElement = try createElement(identifier: identifier, inputFormat: buffer.format)
+                node = try createPlayer(identifier: identifier, inputFormat: buffer.format)
             } catch {
                 errorWriter.writeError(message: error.localizedDescription)
                 return
             }
         }
-        var element = intermediateElement!
 
-        defer {
-            elements[identifier] = element
+        // Get the buffer for this stream.
+        let list = buffer.mutableAudioBufferList.pointee
+
+        guard list.mNumberBuffers == 1 else {
+            fatalError()
         }
 
-        let copied = TPCircularBufferCopyAudioBufferList(&element.buffer,
-                                                         &list,
+        guard list.mBuffers.mDataByteSize > 0 else {
+            fatalError()
+        }
+
+        var mutableList = list
+        self.metrics[identifier]!.copyAttempts += 1
+        let copied = TPCircularBufferCopyAudioBufferList(&buffers[identifier]!,
+                                                         &mutableList,
                                                          nil,
                                                          kTPCircularBufferCopyAll,
                                                          nil)
-        element.metrics.copyAttempts += 1
         guard copied else {
-            element.metrics.copyFails += 1
+            self.metrics[identifier]!.copyFails += 1
             return
         }
     }
 
-    private func createElement(identifier: UInt32, inputFormat: AVAudioFormat) throws -> Element {
-        guard elements[identifier] == nil else { fatalError() }
+    private func createPlayer(identifier: UInt32, inputFormat: AVAudioFormat) throws -> AVAudioSourceNode {
+        guard players[identifier] == nil else { fatalError() }
         print("AudioPlayer => [\(identifier)] New player: \(inputFormat)")
         if !engine.isRunning {
             try engine.start()
         }
 
         // Create the circular buffer.
-        var buffer: TPCircularBuffer = .init()
-        let created = _TPCircularBufferInit(&buffer,
+        buffers[identifier] = .init()
+        let created = _TPCircularBufferInit(&buffers[identifier]!,
                                             1,
                                             MemoryLayout<TPCircularBuffer>.size)
         guard created else {
             fatalError()
         }
 
-        let absd = inputFormat.streamDescription.pointee
+        var absd = inputFormat.streamDescription.pointee
         let node: AVAudioSourceNode = .init(format: inputFormat) { silence, timestamp, numFrames, data in
-            self.renderFunction(silence: silence,
-                                timestamp: timestamp,
-                                numFrames: numFrames,
-                                data: data,
-                                absd: absd,
-                                identifier: identifier)
+
+            // Fill the buffers as best we can.
+            var copiedFrames: UInt32 = numFrames
+            let mutableTimestamp = timestamp
+            TPCircularBufferDequeueBufferListFrames(&self.buffers[identifier]!,
+                                                    &copiedFrames,
+                                                    data,
+                                                    .init(mutating: mutableTimestamp),
+                                                    &absd)
+            self.metrics[identifier]!.reads += 1
+            guard copiedFrames == numFrames else {
+                // Ensure any incomplete data is pure silence.
+                let buffers: UnsafeMutableAudioBufferListPointer = .init(data)
+                for buffer in buffers {
+                    guard let dataPointer = buffer.mData else {
+                        break
+                    }
+                    let discontinuityStartOffset = Int(copiedFrames * absd.mBytesPerFrame)
+                    let numberOfSilenceBytes = Int((numFrames - copiedFrames) * absd.mBytesPerFrame)
+                    guard discontinuityStartOffset + numberOfSilenceBytes == buffer.mDataByteSize else {
+                        print("[FasterAVEngineAudioPlayer] Invalid buffers when calculating silence")
+                        break
+                    }
+                    memset(dataPointer + discontinuityStartOffset, 0, Int(numberOfSilenceBytes))
+                    let thisBufferSilence = numberOfSilenceBytes == buffer.mDataByteSize
+                    let silenceSoFar = silence.pointee.boolValue
+                    silence.pointee = .init(thisBufferSilence && silenceSoFar)
+                    self.metrics[identifier]!.incompleteFrames += 1
+                }
+                return .zero
+            }
+            return .zero
         }
         engine.attach(node)
         engine.connect(node, to: mixer, format: nil)
-        return .init(player: node, buffer: buffer, metrics: .init())
-    }
-
-    // swiftlint:disable function_parameter_count
-    @inline(__always)
-    private func renderFunction(silence: UnsafeMutablePointer<ObjCBool>,
-                                timestamp: UnsafePointer<AudioTimeStamp>,
-                                numFrames: AVAudioFrameCount,
-                                data: UnsafeMutablePointer<AudioBufferList>,
-                                absd: AudioStreamBasicDescription,
-                                identifier: UInt32) -> OSStatus {
-        // Ensure we can access the resources.
-        guard Self.getLock(atomic: &self.elementsLock) == true else {
-            // We didn't get the lock. We'll have to drop this audio.
-            OSAtomicIncrement32(&self.readContentions)
-            Self.fillSilence(data: data,
-                             numFrames: numFrames,
-                             absd: absd,
-                             copiedFrames: 0,
-                             silence: silence)
-            return .zero
-        }
-
-        defer { Self.unlock(atomic: &self.elementsLock) }
-
-        guard var element = elements[identifier] else {
-            fatalError()
-        }
-
-        defer {
-            self.elements[identifier] = element
-        }
-
-        // Fill the buffers as best we can.
-        var copiedFrames: UInt32 = numFrames
-        var absd = absd
-        TPCircularBufferDequeueBufferListFrames(&element.buffer,
-                                                &copiedFrames,
-                                                data,
-                                                .init(mutating: timestamp),
-                                                &absd)
-        element.metrics.reads += 1
-        guard copiedFrames == numFrames else {
-            // Ensure any incomplete data is pure silence.
-            Self.fillSilence(data: data,
-                             numFrames: numFrames,
-                             absd: absd,
-                             copiedFrames: copiedFrames,
-                             silence: silence)
-            element.metrics.incompleteFrames += 1
-            return .zero
-        }
-        return .zero
-    }
-    // swiftlint:enable function_parameter_count
-
-    @inline(__always)
-    private static func fillSilence(data: UnsafeMutablePointer<AudioBufferList>,
-                                    numFrames: AVAudioFrameCount,
-                                    absd: AudioStreamBasicDescription,
-                                    copiedFrames: UInt32,
-                                    silence: UnsafeMutablePointer<ObjCBool>) {
-        let buffers: UnsafeMutableAudioBufferListPointer = .init(data)
-        for buffer in buffers {
-            guard let dataPointer = buffer.mData else {
-                break
-            }
-            let discontinuityStartOffset = Int(copiedFrames * absd.mBytesPerFrame)
-            let numberOfSilenceBytes = Int((numFrames - copiedFrames) * absd.mBytesPerFrame)
-            guard discontinuityStartOffset + numberOfSilenceBytes == buffer.mDataByteSize else {
-                print("[FasterAVEngineAudioPlayer] Invalid buffers when calculating silence")
-                break
-            }
-            memset(dataPointer + discontinuityStartOffset, 0, Int(numberOfSilenceBytes))
-            let thisBufferSilence = numberOfSilenceBytes == buffer.mDataByteSize
-            let silenceSoFar = silence.pointee.boolValue
-            silence.pointee = .init(thisBufferSilence && silenceSoFar)
-        }
+        players[identifier] = node
+        metrics[identifier] = .init()
+        return node
     }
 
     func removePlayer(identifier: UInt32) {
-        removeElement(identifier: identifier, lock: true)
-    }
+        guard let player = players[identifier] else { return }
+        print("Removing \(identifier)")
 
-    private func removeElement(identifier: UInt32, lock: Bool) {
-        if lock {
-            // Spin until we can remove.
-            var contention = false
-            while Self.getLock(atomic: &elementsLock) != true {
-                if !contention {
-                    OSAtomicIncrement32(&self.writeContentions)
-                    contention = true
-                }
-            }
-        }
+        engine.disconnectNodeInput(player)
+        engine.detach(player)
+        players.removeValue(forKey: identifier)
 
-        defer {
-            if lock {
-                Self.unlock(atomic: &elementsLock)
-            }
-        }
+        var buffer = buffers.removeValue(forKey: identifier)!
+        TPCircularBufferCleanup(&buffer)
 
-        guard var element = elements.removeValue(forKey: identifier) else { return }
-        print("[FasterAVAudioEngine] Removing \(identifier)")
-
-        // Dispose of the element's resources.
-        engine.disconnectNodeInput(element.player)
-        engine.detach(element.player)
-        element.player.reset()
-        TPCircularBufferCleanup(&element.buffer)
-
-        // Report metrics on leave.
-        print("They had \(element.metrics.copyFails)/\(element.metrics.copyAttempts) copy fails")
-        print("They had \(element.metrics.incompleteFrames)/\(element.metrics.reads) incomplete reads")
-        print("They had \(readContentions)/\(element.metrics.copyAttempts) contentions")
-        print("They had \(writeContentions) write contentions")
+        let metric = metrics.removeValue(forKey: identifier)!
+        print("They had \(metric.copyFails)/\(metric.copyAttempts) copy fails")
+        print("They had \(metric.incompleteFrames)/\(metric.reads) incomplete reads")
     }
 }
