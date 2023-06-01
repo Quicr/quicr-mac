d296d73a42200b51bc836db74dd52b43d5406314
Rich Logan <rich@richlogan.co.uk>
2023-06-01 20:49:40 +0100

    Remove old audio players (#114)

--- Decimus/Views/Settings/SettingsView.swift
+++ Decimus/Views/Settings/SettingsView.swift
@@ -1,26 +1,9 @@
 import SwiftUI
 
-enum PlayerType: Int, CaseIterable, Identifiable {
-    case audioUnit = 0
-    case avAudioEngine
-    case fasterAvAudioEngine
-    var id: Int { rawValue }
-}
-
 struct SettingsView: View {
-    @AppStorage("playerType")
-    private var playerType: Int = PlayerType.fasterAvAudioEngine.rawValue
 
     var body: some View {
         Form {
-            Section("Audio") {
-                Picker("Player", selection: $playerType) {
-                    ForEach(PlayerType.allCases) { option in
-                        Text(String("\(option)"))
-                    }
-                }
-            }
-
             RelaySettingsView()
                 .textFieldStyle(.roundedBorder)
 
