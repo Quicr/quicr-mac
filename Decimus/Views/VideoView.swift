f82ba5b60822bc11f81fb22bf7c3ea690ea00ce8
Rich Logan <rilogan@cisco.com>
2023-07-17 15:24:40 +0100

    Set video gravity

--- Decimus/Views/VideoView.swift
+++ Decimus/Views/VideoView.swift
@@ -5,6 +5,23 @@ class VideoUIView: UIView {
     override public class var layerClass: AnyClass {
         return AVSampleBufferDisplayLayer.self
     }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeLayer()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initializeLayer()
+    }
+
+    func initializeLayer() {
+        guard let layer = layer as? AVSampleBufferDisplayLayer else {
+            fatalError()
+        }
+        layer.videoGravity = .resizeAspect
+    }
 }
 
 struct VideoView: UIViewRepresentable {
