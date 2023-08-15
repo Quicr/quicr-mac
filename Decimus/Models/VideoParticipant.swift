0b28ab906a2a4f34039c2aed7fb4a1fe5c7afd1f
Rich Logan <rilogan@cisco.com>
2023-08-15 12:21:16 +0100

    Trigger cleanup timer on subscribed object
    - Defensive checks

--- Decimus/Models/VideoParticipant.swift
+++ Decimus/Models/VideoParticipant.swift
@@ -3,6 +3,8 @@ import Combine
 
 enum ParticipantError: Error {
     case notFound
+    case alreadyExists
+    case mainThreadOnly
 }
 
 class VideoParticipant: ObservableObject, Identifiable {
@@ -20,10 +22,15 @@ class VideoParticipants: ObservableObject {
     @Published var participants: [SourceIDType: VideoParticipant] = [:]
     private var cancellables: [SourceIDType: AnyCancellable] = [:]
 
-    func getOrMake(identifier: SourceIDType) -> VideoParticipant {
-        if let participant = participants[identifier] {
-            return participant
-        }
+    func get(identifier: SourceIDType) throws -> VideoParticipant {
+        guard Thread.isMainThread else { throw ParticipantError.mainThreadOnly }
+        guard let participant = participants[identifier] else { throw ParticipantError.notFound }
+        return participant
+    }
+
+    func create(identifier: SourceIDType) throws {
+        guard Thread.isMainThread else { throw ParticipantError.mainThreadOnly }
+        if participants[identifier] != nil { throw ParticipantError.alreadyExists }
 
         let new: VideoParticipant = .init(id: identifier)
         let cancellable = new.objectWillChange.sink(receiveValue: {
@@ -33,10 +40,10 @@ class VideoParticipants: ObservableObject {
         })
         cancellables[identifier] = cancellable
         participants[identifier] = new
-        return new
     }
 
     func removeParticipant(identifier: SourceIDType) throws {
+        guard Thread.isMainThread else { throw ParticipantError.mainThreadOnly }
         let removed = participants.removeValue(forKey: identifier)
         guard removed != nil else { throw ParticipantError.notFound }
         removed!.objectWillChange.send()
