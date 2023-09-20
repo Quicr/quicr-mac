a7b3c7cb60134b27269f79fca4f2f9b074643f44
Rich Logan <rilogan@cisco.com>
2023-09-20 21:37:00 +0100

    Remove uneeded (right now) reconfigure event

--- Decimus/DecimusAudioEngine.swift
+++ Decimus/DecimusAudioEngine.swift
@@ -10,17 +10,12 @@ class DecimusAudioEngine {
                                              interleaved: true)!
 
     private static let logger: DecimusLogger = .init(DecimusAudioEngine.self)
-
-    /// Represents  an event signalling the audio engine has reconfigured.
-    /// Downstream consumers should check the inputFormat, and reconfigure and reconnect any nodes as needed.
-    typealias ReconfigureEvent = () -> Void
     
     /// The AVAudioEngine instance this AudioEngine wraps.
     let engine: AVAudioEngine = .init()
 
     private var blocks: MutableWrapper<[AnyHashable: AVAudioSinkNodeReceiverBlock]> = .init(value: [:])
     private var observer: NSObjectProtocol?
-    private var reconfigureListeners: [AnyHashable: ReconfigureEvent] = [:]
     private let sink: AVAudioSinkNode
     private var stopped: Bool = false
     private var elements: [SourceIDType: AVAudioSourceNode] = [:]
@@ -37,11 +32,6 @@ class DecimusAudioEngine {
         do {
             // Reconfigure ourselves.
             try self.localReconfigure()
-
-            // Reconfigure everyone else.
-            for listener in self.reconfigureListeners {
-                listener.value()
-            }
 
             // Restart if appropriate.
             try self.start()
@@ -156,20 +146,6 @@ class DecimusAudioEngine {
         }
         engine.detach(element)
         Self.logger.info("(\(identifier)) Removed player node")
-    }
-
-    /// Register a handler that will be called when the audio engine reconfigures.
-    /// Any connected blocks will need to be reconnected, and the format may have changed.
-    /// - Parameter id: The identifier for this callback holder.
-    /// - Parameter callback: Handler for the reconfigure event.
-    func registerReconfigureInterest(id: AnyHashable, callback: @escaping ReconfigureEvent) {
-        reconfigureListeners[id] = callback
-    }
-
-    /// Unregister a previously registered reconfigure handler.
-    /// - Parameter id: The identifier for the callback registration.
-    func unregisterReconfigureInterest(id: AnyHashable) {
-        reconfigureListeners.removeValue(forKey: id)
     }
 
     private func localReconfigure() throws {
