e9b14332f1cc277d06372017dde5f7e372dc1997
Rich Logan <rilogan@cisco.com>
2023-10-10 09:59:38 +0100

    Improve test coverage

--- Tests/TestQHelpers.swift
+++ Tests/TestQHelpers.swift
@@ -6,42 +6,69 @@ final class TestQHelpers: XCTestCase {
         let namegate: SequentialObjectBlockingNameGate = .init()
 
         // 0,0 with no history should go through.
-        XCTAssert(namegate.handle(groupId: UInt32.random(in: 0...UInt32.max), objectId: 0, lastGroup: nil, lastObject: nil))
+        XCTAssert(namegate.handle(groupId: random(),
+                                  objectId: 0,
+                                  lastGroup: nil,
+                                  lastObject: nil))
+
         // Non 0 object ID with no history should not go through.
-        XCTAssertFalse(namegate.handle(groupId: UInt32.random(in: 0...UInt32.max), objectId: UInt16.random(in: 1...UInt16.max), lastGroup: nil, lastObject: nil))
+        XCTAssertFalse(namegate.handle(groupId: random(),
+                                       objectId: UInt16.random(in: 1...UInt16.max),
+                                       lastGroup: nil,
+                                       lastObject: nil))
 
         // Sequential object in a group should go through.
         do {
-            let currentGroup: UInt32 = .random(in: 0...UInt32.max)
+            let currentGroup: UInt32 = random()
             let currentObject: UInt16 = .random(in: 0...UInt16.max - 1)
-            XCTAssert(namegate.handle(groupId: currentGroup, objectId: currentObject + 1, lastGroup: currentGroup, lastObject: currentObject))
+            XCTAssert(namegate.handle(groupId: currentGroup,
+                                      objectId: currentObject + 1,
+                                      lastGroup: currentGroup,
+                                      lastObject: currentObject))
             // Sequential objects in a different group should not go through.
             var differentGroup: UInt32
             repeat {
-                differentGroup = .random(in: 0...UInt32.max)
+                differentGroup = random()
             }
             while differentGroup == currentGroup
-            XCTAssertFalse(namegate.handle(groupId: differentGroup, objectId: currentObject + 1, lastGroup: currentGroup, lastObject: currentObject))
+            XCTAssertFalse(namegate.handle(groupId: differentGroup,
+                                           objectId: currentObject + 1,
+                                           lastGroup: currentGroup,
+                                           lastObject: currentObject))
         }
 
         // Non sequential object ID in a group should not go through.
         do {
-            let currentGroup: UInt32 = .random(in: 0...UInt32.max)
+            let currentGroup: UInt32 = random()
             let currentObject: UInt16 = .random(in: 0...UInt16.max - 2)
             let new: UInt16 = .random(in: currentObject+2...UInt16.max)
-            XCTAssertFalse(namegate.handle(groupId: currentGroup, objectId: new, lastGroup: currentGroup, lastObject: currentObject))
+            XCTAssertFalse(namegate.handle(groupId: currentGroup,
+                                           objectId: new,
+                                           lastGroup: currentGroup,
+                                           lastObject: currentObject))
         }
 
         // Incrementing group jumps should go through when their objectId is 0.
         do {
             let currentGroup: UInt32 = .random(in: 0...UInt32.max - 2)
             let nextGroup: UInt32 = .random(in: currentGroup+1...UInt32.max-1)
-            XCTAssert(namegate.handle(groupId: nextGroup, objectId: 0, lastGroup: currentGroup, lastObject: 0))
+            XCTAssert(namegate.handle(groupId: nextGroup,
+                                      objectId: 0,
+                                      lastGroup: currentGroup,
+                                      lastObject: 0))
+
             // Even when it jumps non-sequentially.
-            XCTAssert(namegate.handle(groupId: nextGroup + 1, objectId: 0, lastGroup: currentGroup, lastObject: 0))
+            XCTAssert(namegate.handle(groupId: nextGroup + 1,
+                                      objectId: 0,
+                                      lastGroup: currentGroup,
+                                      lastObject: 0))
+
             // Group jumps should not go through when their objectId is not 0.
             let nonZeroObject: UInt16 = .random(in: 1...UInt16.max)
-            XCTAssertFalse(namegate.handle(groupId: nextGroup, objectId: nonZeroObject, lastGroup: currentGroup, lastObject: UInt16.random(in: 0...UInt16.max)))
+            XCTAssertFalse(namegate.handle(groupId: nextGroup,
+                                           objectId: nonZeroObject,
+                                           lastGroup: currentGroup,
+                                           lastObject: random()))
         }
 
         // Example series.
@@ -53,5 +80,16 @@ final class TestQHelpers: XCTestCase {
         XCTAssertTrue(namegate.handle(groupId: 0, objectId: 4, lastGroup: 0, lastObject: 3))
         XCTAssertFalse(namegate.handle(groupId: 0, objectId: 6, lastGroup: 0, lastObject: 4))
         XCTAssertFalse(namegate.handle(groupId: 0, objectId: 7, lastGroup: 0, lastObject: 4))
+    }
+
+    func testAllowAllNameGate() {
+        XCTAssertTrue(AllowAllNameGate().handle(groupId: random(),
+                                                objectId: random(),
+                                                lastGroup: random(),
+                                                lastObject: random()))
+    }
+
+    private func random<T: FixedWidthInteger>() -> T {
+        T.random(in: T.min...T.max)
     }
 }
