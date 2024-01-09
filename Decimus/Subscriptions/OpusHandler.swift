1dc7ea34335fe18df4b288fa6398927c5f08b200
Rich Logan <rilogan@cisco.com>
2024-01-09 16:52:47 +0000

    Initial pass at extracting audio from subscription

---
+++ Decimus/Subscriptions/OpusHandler.swift
@@ -0,0 +1,214 @@
+import Foundation
+import AVFAudio
+import CoreAudio
+
+enum OpusSubscriptionError: Error {
+    case failedDecoderCreation
+}
+
+private class Weak<T> {
+    var value: T
+    init(value: T) {
+        self.value = value
+    }
+}
+
+class OpusHandler {
+    private static let logger = DecimusLogger(OpusHandler.self)
+    private let sourceId: SourceIDType
+    private var decoder: LibOpusDecoder
+    private let engine: DecimusAudioEngine
+    private let asbd: UnsafeMutablePointer<AudioStreamBasicDescription>
+    private var node: AVAudioSourceNode?
+    private var jitterBuffer: QJitterBuffer
+    private let measurement: OpusSubscription._Measurement?
+    private var underrun: Weak<UInt64> = .init(value: 0)
+    private var callbacks: Weak<UInt64> = .init(value: 0)
+    private let granularMetrics: Bool
+
+    init(sourceId: SourceIDType,
+         engine: DecimusAudioEngine,
+         measurement: OpusSubscription._Measurement?,
+         jitterDepth: TimeInterval,
+         jitterMax: TimeInterval,
+         opusWindowSize: OpusWindowSize,
+         granularMetrics: Bool) throws {
+        self.sourceId = sourceId
+        self.engine = engine
+        self.measurement = measurement
+        self.granularMetrics = granularMetrics
+        self.decoder = try .init(format: DecimusAudioEngine.format)
+
+        // Create the jitter buffer.
+        self.asbd = .init(mutating: decoder.decodedFormat.streamDescription)
+        let opusPacketSize = self.asbd.pointee.mSampleRate * opusWindowSize.rawValue
+        self.jitterBuffer = QJitterBuffer(elementSize: Int(asbd.pointee.mBytesPerPacket),
+                                          packetElements: Int(opusPacketSize),
+                                          clockRate: UInt(asbd.pointee.mSampleRate),
+                                          maxLengthMs: UInt(jitterMax * 1000),
+                                          minLengthMs: UInt(jitterDepth * 1000)) { level, msg, alert in
+            OpusHandler.logger.log(level: DecimusLogger.LogLevel(rawValue: level)!, msg!, alert: alert)
+        }
+
+        // Create the player node.
+        self.node = .init(format: decoder.decodedFormat, renderBlock: renderBlock)
+        try self.engine.addPlayer(identifier: sourceId, node: node!)
+    }
+
+    deinit {
+        // Remove the audio playout.
+        do {
+            try engine.removePlayer(identifier: sourceId)
+        } catch {
+            Self.logger.critical("Couldn't remove player: \(error.localizedDescription)")
+        }
+
+        // Reset the node.
+        node?.reset()
+    }
+
+    func submitEncodedAudio(data: Data, sequence: UInt32, date: Date?) throws {
+        // Generate PLC prior to real decode.
+        let selfPtr: UnsafeMutableRawPointer = Unmanaged.passUnretained(self).toOpaque()
+        jitterBuffer.prepare(UInt(sequence),
+                             concealmentCallback: self.plcCallback,
+                             userData: selfPtr)
+
+        // Decode and queue for playout.
+        let decoded = try decoder.write(data: data)
+        try queueDecodedAudio(buffer: decoded, timestamp: date, sequence: sequence)
+
+        // Metrics.
+        Task(priority: .utility) {
+            await self.measurement?.framesUnderrun(underrun: self.underrun.value, timestamp: date)
+            await self.measurement?.callbacks(callbacks: self.callbacks.value, timestamp: date)
+        }
+    }
+
+    private lazy var renderBlock: AVAudioSourceNodeRenderBlock = { [jitterBuffer, asbd, weak underrun, weak callbacks] silence, _, numFrames, data in
+        // Fill the buffers as best we can.
+        if let callbacks = callbacks {
+            callbacks.value += UInt64(numFrames)
+        }
+        guard data.pointee.mNumberBuffers == 1 else {
+            // Unexpected.
+            let buffers: UnsafeMutableAudioBufferListPointer = .init(data)
+            Self.logger.error("Got multiple buffers: \(data.pointee.mNumberBuffers)")
+            for (idx, buffer) in buffers.enumerated() {
+                Self.logger.error("Buffer \(idx) size: \(buffer.mDataByteSize), channels: \(buffer.mNumberChannels)")
+            }
+            return 1
+        }
+
+        guard data.pointee.mBuffers.mNumberChannels == asbd.pointee.mChannelsPerFrame else {
+            Self.logger.error("Unexpected render block channels. Got \(data.pointee.mBuffers.mNumberChannels). Expected \(asbd.pointee.mChannelsPerFrame)")
+            return 1
+        }
+
+        let buffer: AudioBuffer = data.pointee.mBuffers
+        assert(buffer.mDataByteSize == numFrames * asbd.pointee.mBytesPerFrame)
+        let copiedFrames = jitterBuffer.dequeue(buffer.mData,
+                                                destinationLength: Int(buffer.mDataByteSize),
+                                                elements: Int(numFrames))
+        guard copiedFrames == numFrames else {
+            // Ensure any incomplete data is pure silence.
+            let framesUnderan = UInt64(numFrames) - UInt64(copiedFrames)
+            silence.pointee = .init(framesUnderan == numFrames)
+            if let underrun = underrun {
+                underrun.value += framesUnderan
+            }
+            let buffers: UnsafeMutableAudioBufferListPointer = .init(data)
+            for buffer in buffers {
+                guard let dataPointer = buffer.mData else {
+                    break
+                }
+                let bytesPerFrame = Int(asbd.pointee.mBytesPerFrame)
+                let discontinuityStartOffset = copiedFrames * bytesPerFrame
+                let numberOfSilenceBytes = Int(framesUnderan) * bytesPerFrame
+                guard discontinuityStartOffset + numberOfSilenceBytes == buffer.mDataByteSize else {
+                    Self.logger.error("Invalid buffers when calculating silence")
+                    break
+                }
+                memset(dataPointer + discontinuityStartOffset, 0, Int(numberOfSilenceBytes))
+            }
+            return .zero
+        }
+        return .zero
+    }
+
+    private let plcCallback: PacketCallback = { packets, count, userData in
+        guard let userData = userData else {
+            OpusHandler.logger.error("Expected self in userData")
+            return
+        }
+        let handler: OpusHandler = Unmanaged<OpusHandler>.fromOpaque(userData).takeUnretainedValue()
+        var concealed: UInt64 = 0
+        for index in 0..<count {
+            // Make PLC packets.
+            var packet = packets!.advanced(by: index)
+            do {
+                // TODO: This can be optimized with some further work to decode PLC directly into the buffer.
+                let plcData = try handler.decoder.plc(frames: AVAudioFrameCount(packet.pointee.elements))
+                let list = plcData.audioBufferList
+                guard list.pointee.mNumberBuffers == 1 else {
+                    throw "Not sure what to do with this"
+                }
+
+                // Get audio data as packet list.
+                let audioBuffer = list.pointee.mBuffers
+                guard let data = audioBuffer.mData else {
+                    throw "AudioBuffer data was nil"
+                }
+                assert(packet.pointee.length == audioBuffer.mDataByteSize)
+                memcpy(packet.pointee.data, data, packet.pointee.length)
+                concealed += UInt64(packet.pointee.elements)
+            } catch {
+                OpusHandler.logger.error("\(error.localizedDescription)")
+            }
+        }
+        if let measurement = handler.measurement {
+            let constConcealed = concealed
+            let timestamp: Date? = handler.granularMetrics ? .now : nil
+            Task(priority: .utility) {
+                await measurement.concealmentFrames(concealed: constConcealed, timestamp: timestamp)
+            }
+        }
+    }
+
+    private func queueDecodedAudio(buffer: AVAudioPCMBuffer, timestamp: Date?, sequence: UInt32) throws {
+        // Ensure this buffer looks valid.
+        let list = buffer.audioBufferList
+        guard list.pointee.mNumberBuffers == 1 else {
+            throw "Unexpected number of buffers"
+        }
+
+        // Get audio data as packet list.
+        let audioBuffer = list.pointee.mBuffers
+        guard let data = audioBuffer.mData else {
+            Self.logger.error("AudioBuffer data was nil")
+            return
+        }
+
+        let packet: Packet = .init(sequence_number: UInt(sequence),
+                                   data: data,
+                                   length: Int(audioBuffer.mDataByteSize),
+                                   elements: Int(buffer.frameLength))
+
+        let selfPtr: UnsafeMutableRawPointer = Unmanaged.passUnretained(self).toOpaque()
+
+        // Copy in.
+        let copied = jitterBuffer.enqueue(packet,
+                                          concealmentCallback: self.plcCallback,
+                                          userData: selfPtr)
+
+        let missing = copied < buffer.frameLength ? Int(buffer.frameLength) - copied : 0
+        if let measurement = measurement {
+            Task(priority: .utility) {
+                await measurement.receivedFrames(received: buffer.frameLength, timestamp: timestamp)
+                await measurement.recordLibJitterMetrics(metrics: jitterBuffer.getMetrics(),
+                                                         timestamp: timestamp)
+                await measurement.droppedFrames(dropped: missing, timestamp: timestamp)
+            }
+        }
+    }
+}
