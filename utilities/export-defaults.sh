a979385f61fe546138f6e2e54e39d86f89ee67d9
Rich Logan <rich@richlogan.co.uk>
2024-10-28 14:56:25 +0000

    Add utility scripts

---
+++ utilities/export-defaults.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+OUTPUT="${1:-defaults.json}"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+defaults export com.cisco.quicr.decimus $SCRIPT_DIR/defaults
+plutil -convert json -o $SCRIPT_DIR/$OUTPUT $SCRIPT_DIR/defaults
+rm $SCRIPT_DIR/defaults
---
+++ utilities/import-defaults.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+INPUT="${1:-defaults.json}"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+plutil -convert xml1 -o $SCRIPT_DIR/defaults $SCRIPT_DIR/$INPUT
+defaults import com.cisco.quicr.decimus $SCRIPT_DIR/defaults
+rm $SCRIPT_DIR/defaults
