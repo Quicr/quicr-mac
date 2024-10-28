e97b006ad5f75dd94a9782cb79a2eda0a64d4529
Rich Logan <rich@richlogan.co.uk>
2024-10-28 15:05:17 +0000

    Fix path

--- utilities/export-defaults.sh
+++ utilities/export-defaults.sh
@@ -3,5 +3,5 @@
 OUTPUT="${1:-defaults.json}"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 defaults export com.cisco.quicr.decimus $SCRIPT_DIR/defaults
-plutil -convert json -o $SCRIPT_DIR/$OUTPUT $SCRIPT_DIR/defaults
+plutil -convert json -o $OUTPUT $SCRIPT_DIR/defaults
 rm $SCRIPT_DIR/defaults
--- utilities/import-defaults.sh
+++ utilities/import-defaults.sh
@@ -2,6 +2,6 @@
 
 INPUT="${1:-defaults.json}"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-plutil -convert xml1 -o $SCRIPT_DIR/defaults $SCRIPT_DIR/$INPUT
+plutil -convert xml1 -o $SCRIPT_DIR/defaults $INPUT
 defaults import com.cisco.quicr.decimus $SCRIPT_DIR/defaults
 rm $SCRIPT_DIR/defaults
