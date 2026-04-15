1de88c2d5a34b08b25da61bc7ad9335f31704458
Rich Logan <rich@richlogan.co.uk>
2026-04-15 08:55:19 +0100

    Redo mutex wrappers

--- Decimus/Mutex+Convenience.swift
+++ Decimus/Mutex+Convenience.swift
@@ -8,20 +8,18 @@ extension Mutex {
     public func get() -> Value {
         self.withLock { $0 }
     }
-}
 
-extension Mutex where Value: ExpressibleByNilLiteral {
     /// Set the value of the mutex to nil.
-    public func clear() {
-        self.withLock { $0 = nil }
+    public func clear<Wrapped>() where Value == Wrapped? {
+        self.withLock { $0 = .none }
     }
 
     /// Consume the value of the mutex, setting it to nil.
     /// - Returns The current value.
-    public func consume() -> Value {
+    public func consume<Wrapped>() -> Value where Value == Wrapped? {
         self.withLock { locked in
             let consumed = locked
-            locked = nil
+            locked = .none
             return consumed
         }
     }
