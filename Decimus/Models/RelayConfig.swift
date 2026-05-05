8ceffc9275374913a1f41a49f508ba1e5dab8bf3
Rich Logan <rich@richlogan.co.uk>
2026-05-05 15:29:05 +0100

    Switch to public relay

--- Decimus/Models/RelayConfig.swift
+++ Decimus/Models/RelayConfig.swift
@@ -4,7 +4,7 @@
 /// Connection details for a MoQ relay.
 struct RelayConfig: Codable {
     /// FQDN.
-    var address: String = "moq://us-ord-1.moqx.akaleapi.net:4433/moq-relay"
+    var address: String = "moq://demo.relay.quicr.org:33437"
     /// mDNS Type for lookup.
     var mDNSType: String = "_laps._udp"
     /// True if we should use mDNS to fill relay info.
